848a92d16ffa960ec2c8b3de7c310b8c59f3ea48
wicsion <goldin301@gmail.com>
2025-10-24 01:19:19 +0300

    sada

--- accounts/templatetags/custom_filters.py
+++ accounts/templatetags/custom_filters.py
@@ -9,3 +9,23 @@ def space_format(value):
 @register.filter
 def subtract(value, arg):
     return value - arg
+
+
+@register.filter
+def space_format(value):
+    """
+    Форматирует число с пробелами в качестве разделителей тысяч.
+    """
+    if value is None:
+        return ""
+
+    try:
+        # Преобразуем в число
+        num_value = float(value)
+
+        # Форматируем как целое число
+        return "{:,.0f}".format(num_value).replace(",", " ")
+
+    except (ValueError, TypeError):
+        # Если ошибка, возвращаем исходное значение
+        return str(value)
